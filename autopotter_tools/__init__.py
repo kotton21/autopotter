15fc49a50fd81f1cff039b7ea5c750ab4365c2e1
Karl Bayer <karlsbayer@gmail.com>
2025-08-27 14:49:29 -0400

    __init__.py

--- autopotter_tools/__init__.py
+++ autopotter_tools/__init__.py
@@ -4,21 +4,7 @@ Autopotter Tools Package
 A collection of tools for managing GPT interactions, Instagram APIs, and other automation tasks.
 """
 
-# Import and expose main functions/classes for easier access
-# from .logger import get_logger
-# from .instagram_api import InstagramConfig, InstagramVideoUploader
-# from .json2video_api import Json2VideoConfig, Json2VideoAPI
-# from .instagram_analytics import InstagramAnalyticsManager
-# # from .googlestorage_api import GCSClient
+# Make autopotter_tools a proper Python package
+from .logger import get_logger
 
-# __all__ = [
-#     'get_logger',
-#     'InstagramConfig',
-#     'InstagramVideoUploader',
-#     'Json2VideoConfig',
-#     'Json2VideoAPI',
-#     'InstagramAnalyticsManager',
-#     'GCSClient'
-# ]
-
-# __version__ = "1.0.0"
+__all__ = ['get_logger']
